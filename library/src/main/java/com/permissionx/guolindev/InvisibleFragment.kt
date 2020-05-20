da1b1ca52ee3d6f61f8eceab93a76d18349e3706
guolindev <sinyu890807@163.com>
2020-05-20 20:48:56 +0800

    Correct comments.

--- library/src/main/java/com/permissionx/guolindev/InvisibleFragment.kt
+++ library/src/main/java/com/permissionx/guolindev/InvisibleFragment.kt
@@ -183,7 +183,7 @@ class InvisibleFragment : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == SETTINGS_CODE) {
             // When user switch back from settings, just request again.
-            if (::permissionBuilder.isInitialized) { // On some phones, when switch back from settings, permissionBuilder will become uninitialized
+            if (::permissionBuilder.isInitialized) { // On some phones, when switch back from settings, permissionBuilder may become uninitialized
                 permissionBuilder.requestAgain(permissionBuilder.forwardPermissions)
             } else {
                 Log.w("PermissionX", "permissionBuilder should not be uninitialized at this time, so we can do nothing in this case.")
