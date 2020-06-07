f08866593ecf7f04c15046b2c6d8cf0f8fea9463
guolindev <sinyu890807@163.com>
2020-06-07 10:26:04 +0800

    Distinct the permissions that developers passed in for safety.

--- library/src/main/java/com/permissionx/guolindev/PermissionCollection.java
+++ library/src/main/java/com/permissionx/guolindev/PermissionCollection.java
@@ -3,7 +3,9 @@ package com.permissionx.guolindev;
 import androidx.fragment.app.FragmentActivity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * An internal class to provide specific scope for passing permissions param.
@@ -21,7 +23,27 @@ public class PermissionCollection {
      * @param permissions A vararg param to pass permissions.
      */
     public PermissionBuilder permissions(String... permissions)  {
-        return new PermissionBuilder(activity, new ArrayList<>(Arrays.asList(permissions)));
+        return new PermissionBuilder(activity, distinctPermissions(permissions));
+    }
+
+    /**
+     * Distinct the permissions that passed in. Duplicate permissions can lead to the wrong answer.
+     * @param permissions
+     *          Permissions that app want to request.
+     * @return A filtered permission list with no duplicate value.
+     */
+    private List<String> distinctPermissions(String... permissions) {
+        List<String> uniquePermissions = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        if (permissions != null) {
+            for (String permission : permissions) {
+                if (!set.contains(permission)) {
+                    set.add(permission);
+                    uniquePermissions.add(permission);
+                }
+            }
+        }
+        return uniquePermissions;
     }
 
 }
