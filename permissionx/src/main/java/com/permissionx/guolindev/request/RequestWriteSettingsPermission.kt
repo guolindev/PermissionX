4baf61dff08db05f487d164bb91790fb8688eba2
guolindev <sinyu890807@qq.com>
2021-08-31 21:16:56 +0800

    Refactor RequestWriteSettingsPermission from java to kotlin.

--- permissionx/src/main/java/com/permissionx/guolindev/request/RequestWriteSettingsPermission.kt
+++ permissionx/src/main/java/com/permissionx/guolindev/request/RequestWriteSettingsPermission.kt
@@ -13,15 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.permissionx.guolindev.request
 
-package com.permissionx.guolindev.request;
-
-import android.Manifest;
-import android.os.Build;
-import android.provider.Settings;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.Manifest
+import android.os.Build
+import android.provider.Settings
 
 /**
  * Implementation for request android.permission.WRITE_SETTINGS.
@@ -29,52 +25,45 @@ import java.util.List;
  * @author guolin
  * @since 2021/2/21
  */
-public class RequestWriteSettingsPermission extends BaseTask {
+internal class RequestWriteSettingsPermission internal constructor(permissionBuilder: PermissionBuilder) :
+    BaseTask(permissionBuilder) {
 
-    RequestWriteSettingsPermission(PermissionBuilder permissionBuilder) {
-        super(permissionBuilder);
-    }
-
-    @Override
-    public void request() {
+    override fun request() {
         if (pb.shouldRequestWriteSettingsPermission()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && pb.getTargetSdkVersion() >= Build.VERSION_CODES.M) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && pb.targetSdkVersion >= Build.VERSION_CODES.M) {
                 if (Settings.System.canWrite(pb.activity)) {
                     // WRITE_SETTINGS permission has already granted, we can finish this task now.
-                    finish();
-                    return;
+                    finish()
+                    return
                 }
                 if (pb.explainReasonCallback != null || pb.explainReasonCallbackWithBeforeParam != null) {
-                    List<String> requestList = new ArrayList<>();
-                    requestList.add(Manifest.permission.WRITE_SETTINGS);
+                    val requestList = mutableListOf(Manifest.permission.WRITE_SETTINGS)
                     if (pb.explainReasonCallbackWithBeforeParam != null) {
                         // callback ExplainReasonCallbackWithBeforeParam prior to ExplainReasonCallback
-                        pb.explainReasonCallbackWithBeforeParam.onExplainReason(getExplainScope(), requestList, true);
+                        pb.explainReasonCallbackWithBeforeParam!!.onExplainReason(explainScope, requestList, true)
                     } else {
-                        pb.explainReasonCallback.onExplainReason(getExplainScope(), requestList);
+                        pb.explainReasonCallback!!.onExplainReason(explainScope, requestList)
                     }
                 } else {
                     // No implementation of explainReasonCallback, we can't request
                     // WRITE_SETTINGS permission at this time, because user won't understand why.
-                    finish();
+                    finish()
                 }
             } else {
                 // WRITE_SETTINGS permission is automatically granted below Android M.
-                pb.grantedPermissions.add(Manifest.permission.WRITE_SETTINGS);
+                pb.grantedPermissions.add(Manifest.permission.WRITE_SETTINGS)
                 // At this time, WRITE_SETTINGS permission shouldn't be special treated anymore.
-                pb.specialPermissions.remove(Manifest.permission.WRITE_SETTINGS);
-                finish();
+                pb.specialPermissions.remove(Manifest.permission.WRITE_SETTINGS)
+                finish()
             }
         } else {
             // shouldn't request WRITE_SETTINGS permission at this time, so we call finish() to finish this task.
-            finish();
+            finish()
         }
     }
 
-    @Override
-    public void requestAgain(List<String> permissions) {
+    override fun requestAgain(permissions: List<String>) {
         // don't care what the permissions param is, always request WRITE_SETTINGS permission.
-        pb.requestWriteSettingsPermissionNow(this);
+        pb.requestWriteSettingsPermissionNow(this)
     }
-
 }
