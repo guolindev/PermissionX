c39f43cfd066428b9e395f87efa8bfd2b30c1706
guolindev <sinyu890807@qq.com>
2021-08-31 21:21:59 +0800

    Refactor RequestNormalPermissions from java to kotlin.

--- permissionx/src/main/java/com/permissionx/guolindev/request/RequestNormalPermissions.kt
+++ permissionx/src/main/java/com/permissionx/guolindev/request/RequestNormalPermissions.kt
@@ -13,70 +13,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.permissionx.guolindev.request
 
-package com.permissionx.guolindev.request;
-
-import com.permissionx.guolindev.PermissionX;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import com.permissionx.guolindev.PermissionX
+import java.util.*
 
 /**
  * Implementation for request normal permissions.
  * @author guolin
  * @since 2020/6/10
  */
-public class RequestNormalPermissions extends BaseTask {
+internal class RequestNormalPermissions internal constructor(permissionBuilder: PermissionBuilder) :
+    BaseTask(permissionBuilder) {
 
-    RequestNormalPermissions(PermissionBuilder permissionBuilder) {
-        super(permissionBuilder);
-    }
-
-    @Override
-    public void request() {
-        List<String> requestList = new ArrayList<>();
-        for (String permission : pb.normalPermissions) {
+    override fun request() {
+        val requestList = ArrayList<String>()
+        for (permission in pb.normalPermissions) {
             if (PermissionX.isGranted(pb.activity, permission)) {
-                pb.grantedPermissions.add(permission); // already granted
+                pb.grantedPermissions.add(permission) // already granted
             } else {
-                requestList.add(permission); // still need to request
+                requestList.add(permission) // still need to request
             }
         }
         if (requestList.isEmpty()) { // all permissions are granted
-            finish();
-            return;
+            finish()
+            return
         }
         if (pb.explainReasonBeforeRequest && (pb.explainReasonCallback != null || pb.explainReasonCallbackWithBeforeParam != null)) {
-            pb.explainReasonBeforeRequest = false;
-            pb.deniedPermissions.addAll(requestList);
+            pb.explainReasonBeforeRequest = false
+            pb.deniedPermissions.addAll(requestList)
             if (pb.explainReasonCallbackWithBeforeParam != null) {
                 // callback ExplainReasonCallbackWithBeforeParam prior to ExplainReasonCallback
-                pb.explainReasonCallbackWithBeforeParam.onExplainReason(getExplainScope(), requestList, true);
+                pb.explainReasonCallbackWithBeforeParam!!.onExplainReason(explainScope, requestList, true)
             } else {
-                pb.explainReasonCallback.onExplainReason(getExplainScope(), requestList);
+                pb.explainReasonCallback!!.onExplainReason(explainScope, requestList)
             }
         } else {
             // Do the request at once. Always request all permissions no matter they are already granted or not, in case user turn them off in Settings.
-            pb.requestNow(pb.normalPermissions, this);
+            pb.requestNow(pb.normalPermissions, this)
         }
     }
 
     /**
-     * If permission is denied by user and {@link ExplainScope#showRequestReasonDialog(List, String, String)} or {@link ForwardScope#showForwardToSettingsDialog(List, String, String)} is called,
+     * If permission is denied by user and [ExplainScope.showRequestReasonDialog] or [ForwardScope.showForwardToSettingsDialog] is called,
      * when user clicked positive button, will call this method.
      * @param permissions   permissions to request again.
      */
-    @Override
-    public void requestAgain(List<String> permissions) {
-        Set<String> permissionsToRequestAgain = new HashSet<>(pb.grantedPermissions);
-        permissionsToRequestAgain.addAll(permissions);
-        if (!permissionsToRequestAgain.isEmpty()) {
-            pb.requestNow(permissionsToRequestAgain, this);
+    override fun requestAgain(permissions: List<String>) {
+        val permissionsToRequestAgain: MutableSet<String> = HashSet(pb.grantedPermissions)
+        permissionsToRequestAgain.addAll(permissions)
+        if (permissionsToRequestAgain.isNotEmpty()) {
+            pb.requestNow(permissionsToRequestAgain, this)
         } else {
-            finish();
+            finish()
         }
     }
-
 }
